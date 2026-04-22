20d075e210a9a39041e293e27d14ac4119956cb1
lucashrv <lucas.hrv@outlook.com>
2026-04-22 16:53:18 -0300

    chore: prisma configuration

---
+++ prisma/migrations/20260413105915_user_v1/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('ADMIN', 'INSTITUTE', 'ADOPTER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "roles" "Roles"[],
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "emailVerifiedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
