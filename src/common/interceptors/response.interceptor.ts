8a6cda29b45b25d89b9dccfaa3d81b9f165a6612
lucashrv <lucas.hrv@outlook.com>
2026-04-22 17:31:08 -0300

    feat: Response model

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Response } from 'express';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    return next.handle().pipe(
+      map(data => {
+        if (data?.success !== undefined) {
+          return data;
+        }
+        return {
+          success: true,
+          statusCode: response.statusCode,
+          timestamp: new Date().toISOString(),
+          data,
+        };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from 'common/filters/global-exception.filter';
+import { ResponseInterceptor } from 'common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
