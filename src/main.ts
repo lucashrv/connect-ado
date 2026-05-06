c3099cd657d2b92fa1ed7bb6cd7bcc791cb0f99e
lucashrv <lucas.hrv@outlook.com>
2026-05-06 15:56:45 -0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { ResponseInterceptor } from 'common/interceptors/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
 
