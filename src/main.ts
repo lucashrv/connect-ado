d65454bbc9b06533976cb01197200e32bef7f0b6
lucashrv <lucas.hrv@outlook.com>
2026-05-06 21:27:46 -0300

    chore: add netlify cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://connect-ado.netlify.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
